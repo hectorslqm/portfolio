2e2dded99701c24ab39e70ea09ac7d88178e28f4
Hector Lazcano <hectorslqm@gmail.com>
2025-02-28 21:05:56 -0600

    installed jetbrains font and schadcn/ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,15 @@
+import React from "react";
 import Image from "next/image";
 
-export default function Home() {
+export default function HomePage() {
   return (
-    <>Home</>
-    );
+    <div>
+      <h1>
+        Welcome to My Portfolio
+      </h1>
+      <p>
+        This is a sample text using JetBrains Mono font.
+      </p><b>sami</b>
+    </div>
+  );
 }
