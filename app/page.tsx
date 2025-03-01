8e67e0536500f710e0c3f3ae254ab2ffaa8c9d83
Hector Lazcano <hectorslqm@gmail.com>
2025-02-28 21:34:57 -0600

    reset the project

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,13 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <>Home</>
+    <div>
+      <h1>
+        Welcome to My Portfolio
+      </h1>
+      <p>
+        This is a sample text using JetBrains Mono font.
+      </p><b>sami</b>
+    </div>
     );
 }
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,15 @@
 function Navbar() {
     return(
-        <></>
+        <nav className="bg-slate-200 flex rounded-b-sm px-2">
+            <div className="flex flex-none">
+                Hector Lazcano
+            </div>
+            <div  className="flex flex-auto justify-end space-x-2">
+                <a className="btn">Projects</a>
+                <a className="btn">Contact</a>
+                <a className="btn">Contact</a>
+            </div>
+        </nav>
     )
 }
 export default Navbar;
