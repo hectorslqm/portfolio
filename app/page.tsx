37f9bab351b2d16ddb4e509d1db3e3aa99a725e2
Hector Lazcano <hectorslqm@gmail.com>
2025-03-01 13:29:39 -0600

    update navBar

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,12 @@
+import React from "react";
 import Image from "next/image";
 
-export default function Home() {
+export default function HomePage() {
   return (
     <div>
-      <h1>
-        Welcome to My Portfolio
-      </h1>
       <p>
-        This is a sample text using JetBrains Mono font.
-      </p><b>sami</b>
+        Personal portfolio
+      </p>
     </div>
-    );
+  );
 }
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,15 +1,58 @@
+import Link from 'next/link';
+
 function Navbar() {
     return(
-        <nav className="bg-slate-200 flex rounded-b-sm px-2">
-            <div className="flex flex-none">
-                Hector Lazcano
+        <nav className="relative flex flex-wrap bg-teal-500 p-2 shadow-xl">           
+            {/* Menú para pantallas grandes (lg) */}
+            <div className="hidden flex-grow lg:flex lg:items-center lg:w-auto space-x-4">
+                <div>
+                    <Link href="#" className="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0">
+                        Download CV
+                    </Link>
+                </div>
+                <div className="text-md space-x-3">
+                    <Link href="#responsive-header" className="block lg:inline-block lg:mt-0 text-teal-200 hover:text-white">
+                        Portfolio
+                    </Link>
+                    <Link href="#responsive-header" className="block lg:inline-block lg:mt-0 text-teal-200 hover:text-white">
+                        Work
+                    </Link>
+                    <Link href="#responsive-header" className="block lg:inline-block lg:mt-0 text-teal-200 hover:text-white">
+                        Contact
+                    </Link>
+                </div>
+                
             </div>
-            <div  className="flex flex-auto justify-end space-x-2">
-                <a className="btn">Projects</a>
-                <a className="btn">Contact</a>
-                <a className="btn">Contact</a>
+            
+            {/* Botón del menú para dispositivos móviles */}
+            <div className="block lg:hidden">
+                <label htmlFor="menu-toggle" className="cursor-pointer flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
+                    <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
+                </label>
+                <input type="checkbox" id="menu-toggle" className="hidden peer" />
+                {/* Menú desplegable para dispositivos móviles */}
+                <div className="absolute left-0 top-full mt-0.5 hidden peer-checked:block bg-teal-500 shadow-2xl rounded-lg py-2 px-4 z-10 w-36 ml-0.5">
+                    <Link href="#responsive-header" className="block text-sm py-2 text-white hover:text-teal-200">
+                        Portfolio
+                    </Link>
+                    <Link href="#responsive-header" className="block text-sm py-2 text-white hover:text-teal-200">
+                        Work
+                    </Link>
+                    <Link href="#responsive-header" className="block text-sm py-2 text-white hover:text-teal-200">
+                        Contact
+                    </Link>
+                    <div className="border-t border-teal-400 my-2"></div>
+                    <Link href="#" className="block text-sm py-2 text-white hover:text-teal-200">
+                        Download CV
+                    </Link>
+                </div>
+            </div>
+            <div className="flex items-center flex-grow justify-end text-white mr-1">
+                <span className="font-semibold text-lg tracking-tight">Hector S. Lazcano</span>
             </div>
         </nav>
+        
     )
 }
+
 export default Navbar;
