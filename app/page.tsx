d0cbaeb9fa78403c327ab9175bc7cea6093d31b6
Hector Lazcano <hectorslqm@gmail.com>
2025-02-28 21:32:41 -0600

    Revert "installed jetbrains font and schadcn/ui"
    This reverts commit 2e2dded99701c24ab39e70ea09ac7d88178e28f4.

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,7 @@
-import React from "react";
 import Image from "next/image";
 
-export default function HomePage() {
+export default function Home() {
   return (
-    <div>
-      <h1>
-        Welcome to My Portfolio
-      </h1>
-      <p>
-        This is a sample text using JetBrains Mono font.
-      </p><b>sami</b>
-    </div>
-  );
+    <>Home</>
+    );
 }
