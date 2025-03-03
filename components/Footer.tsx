2817f08b8f4cf151d9b005d1f5eff35096a31ef1
hectorslqm <hectorslqm@gmail.com>
2025-03-03 15:44:50 -0600

    add a dessign to the main layout, added waves, added blur to the navbar and created the footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,38 @@
 function Footer() {
   return (
-    <></>
+    <footer className="absolute bottom-0 w-full py-2 text-white/80">
+      <div className="container mx-auto px-4 flex justify-between items-center">
+        {/* Copyright */}
+        <div className="text-lg">
+          <p>© {new Date().getFullYear()} Hector S. Lazcano</p>
+        </div>
+
+        {/*Contact*/}
+        <div className="text-sm space-y-2">
+          <p>
+            <a
+              href="https://www.linkedin.com/in/h%C3%A9ctor-samuel-lazcano-quintero-m%C3%A1rmol-48a766148/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="hover:text-white transition-colors"
+            >
+              LinkedIn
+            </a>
+          </p>
+          <p>
+            <a href="tel:+19793262670" className="hover:text-white transition-colors">
+              +1 (979) 3 26 26 70
+            </a>
+          </p>
+          <p>
+            <a href="mailto:hectorslqm@gmail.com" className="hover:text-white transition-colors">
+              hectorslqm@gmail.com
+            </a>
+          </p>
+          <p>College Station, TX</p>
+        </div>
+      </div>
+    </footer>
   );
 }
 export default Footer;
